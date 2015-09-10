0762a928d39211cbae2487e5ace2938a28df3148
robmadeyou <robmadeyou@gmail.com>
2015-09-10 17:26:04 +0100

    Changing title and allowing editing of forms

--- src/Presenters/Gallery/GalleryEditView.php
+++ src/Presenters/Gallery/GalleryEditView.php
@@ -2,7 +2,16 @@
 
 namespace Your\WebApp\Presenters\Gallery;
 
+use Rhubarb\Crown\Settings\HtmlPageSettings;
+
 class GalleryEditView extends GalleryAddView
 {
+    protected function printViewContent()
+    {
+        parent::printViewContent();
+
+        $html = new HtmlPageSettings();
+        $html->PageTitle =  'Pievienot vel bildes galerija';
+    }
 
 }
